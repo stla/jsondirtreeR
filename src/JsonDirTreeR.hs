05efe79cc9e72a83cf8d33552a214a1de250b89f
stla <laurent_step@yahoo.fr>
2017-02-24 00:01:55 +0100

    corrections

--- src/JsonDirTreeR.hs
+++ src/JsonDirTreeR.hs
@@ -1,13 +1,19 @@
 -- compilation
--- first: stack install
--- second (ignore the error 'ghc can't apply -o to multiple source files'):
 -- Windows:
 --   64 bit:
---     stack build --ghc-options "-shared -fPIC StartEnd.c -o Rpackage/inst/libs/x64/JsonDirTree.dll"
+--     stack setup
+--     stack exec -- ghc -c StartEnd.c
+--     stack build --ghc-options "-shared -fPIC -o JsonDirTreeR.dll C:\HaskellProjects\jsondirtreeR\StartEnd.o"
+--     mv JsonDirTreeR.dll Rpackage/inst/libs/x64/JsonDirTreeR.dll
+--     rm JsonDirTreeR.dll.a
 --   32 bit:
+--     stack clean
 --     stack install jsondirtree --stack-yaml stack32jsondirtree.yaml
 --     stack setup --stack-yaml stack32.yaml
---     stack build --stack-yaml stack32.yaml --ghc-options "-shared -fPIC StartEnd.c -o Rpackage/inst/libs/i386/JsonDirTree.dll"
+--     stack exec -- ghc -c StartEnd.c
+--     stack build --stack-yaml stack32.yaml --ghc-options "-shared -fPIC -o JsonDirTreeR.dll C:\HaskellProjects\jsondirtreeR\StartEnd.o"
+--     mv JsonDirTreeR.dll Rpackage/inst/libs/i386/JsonDirTreeR.dll
+--     rm JsonDirTreeR.dll.a
 -- Linux:
 --
 
@@ -23,15 +29,14 @@ import Foreign.C
 import JsonDirTree (dirToJSONtree)
 import Data.ByteString.Lazy.Internal (unpackChars)
 
-main = putStrLn "hello"
-
 foreign export ccall dirToJSONtreeR :: Ptr CInt -> Ptr CString -> Ptr CString -> IO ()
 dirToJSONtreeR :: Ptr CInt -> Ptr CString -> Ptr CString -> IO ()
 dirToJSONtreeR depth dir result = do
   depth <- peek depth
+  let d = fromIntegral depth :: Int
   dir <- (>>=) (peek dir) peekCString
   json <- dirToJSONtree
-            (if depth<0 then Nothing else Just (fromIntegral depth :: Int))
+            (if d<0 then Nothing else Just d)
               dir
   jsonC <- newCString $ unpackChars json
   poke result $ jsonC
